866040e25c856703ce7be30a37ed0129f807f7e8
idid10 <ejwuud2@naver.com>
2025-01-24 01:41:21 +0900

    initSetting

--- lib/main.dart
+++ lib/main.dart
@@ -36,7 +36,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
         useMaterial3: true,
       ),
-      home: const WholeSchedulePage(),
+      home: const LoginPage(),
     );
   }
 }
